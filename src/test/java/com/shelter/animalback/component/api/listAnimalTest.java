56bdd3041feafbb6620395d36ddedc6e0e8be6d6
7uanpablo <jpmm1997@hotmail.com>
2021-04-12 16:23:15 -0500

    refactor(test): use mvcMokc listAnimalTest

--- src/test/java/com/shelter/animalback/component/api/listAnimalTest.java
+++ src/test/java/com/shelter/animalback/component/api/listAnimalTest.java
@@ -1,57 +1,72 @@
 package com.shelter.animalback.component.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shelter.animalback.controller.dto.AnimalDto;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
+import lombok.SneakyThrows;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 public class listAnimalTest {
-    @LocalServerPort
-    private int port;
-    private AnimalDto cat;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @BeforeEach
+    @SneakyThrows
     public void setUp() {
-        RestAssured.port = port;
-         cat = new AnimalDto("Thor", "Birmano", "Male", false, new String[]{"Leucemia Felina"});
-
-        RestAssured
-                .given()
-                .contentType(ContentType.JSON)
-                .body(cat)
-                .when()
-                .post("/animals");
+        AnimalDto  cat = new AnimalDto("Thor", "Birmano", "Male", false, new String[]{"Leucemia Felina"});
+        var carString = new ObjectMapper().writeValueAsString(cat);
+        mockMvc.perform(post("/animals")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(carString));
     }
 
     @Test
+    @SneakyThrows
     public void listAnimalsSuccessfully() {
-        RestAssured
-                .when()
-                .get("/animals")
-                .then().assertThat()
-                .statusCode(200)
-                .and()
-                .contentType(ContentType.JSON);
+
+        var response = mockMvc.perform(get("/animals")).andReturn().getResponse();
+
+        assertThat(response.getStatus(), equalTo(HttpStatus.OK.value()));
+        assertThat(response.getContentType(), equalTo(MediaType.APPLICATION_JSON.toString()));
     }
 
     @Test
+    @SneakyThrows
     public void listAnimalsWithRightSchema() {
-        RestAssured
-                .when()
-                .get("/animals")
-                .then().assertThat()
-                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("./animals.json"));
+
+        var response = mockMvc.perform(get("/animals")).andReturn().getResponse();
+
+        var jsonSchema = new JSONObject(new JSONTokener(listAnimalTest.class.getResourceAsStream("/animals.json")));
+        var jsonArray = new JSONArray(response.getContentAsString());
+
+        var schema = SchemaLoader.load(jsonSchema);
+        schema.validate(jsonArray);
     }
 }
