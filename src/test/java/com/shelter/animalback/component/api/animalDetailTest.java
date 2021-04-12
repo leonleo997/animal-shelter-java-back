f35038ff36afef832a15ba1f04f1d3e5f662c831
7uanpablo <jpmm1997@hotmail.com>
2021-04-12 15:08:06 -0500

    refactor(test): use mvcMokc in animaDetailWithsuccesStatusCodeAndContentType test 2

--- src/test/java/com/shelter/animalback/component/api/animalDetailTest.java
+++ src/test/java/com/shelter/animalback/component/api/animalDetailTest.java
@@ -4,13 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shelter.animalback.controller.dto.AnimalDto;
 import com.shelter.animalback.domain.Animal;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -18,17 +18,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
 public class animalDetailTest {
 
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private MockMvc mockMvc;
@@ -37,7 +36,7 @@ public class animalDetailTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        RestAssured.port = port;
+        
         cat = new AnimalDto("Thor", "Birmano", "Male", false, new String[]{"Leucemia Felina"});
         var carString = new ObjectMapper().writeValueAsString(cat);
         mockMvc.perform(post("/animals")
@@ -54,12 +53,10 @@ public class animalDetailTest {
     }
 
     @Test
-    public void detailWithTheRightAnimal() {
-        Animal animal =
-                RestAssured
-                        .when()
-                        .get("/animals/Thor")
-                        .then().extract().as(Animal.class);
+    public void detailWithTheRightAnimal() throws Exception {
+        var response = mockMvc.perform(get("/animals/Thor")).andReturn().getResponse();
+
+        var animal = new ObjectMapper().readValue(response.getContentAsString(),Animal.class);
 
         MatcherAssert.assertThat(animal.getName(), Matchers.equalTo(cat.getName()));
         MatcherAssert.assertThat(animal.getBreed(), Matchers.equalTo(cat.getBreed()));
