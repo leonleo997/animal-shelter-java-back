c8a082023e206357477199f24d3ce55852499d3c
Yesid Leonardo Lopez Sierra <ylopezs@psl.com.co>
2021-04-27 23:35:04 -0500

    update contract test

--- src/test/java/com/shelter/animalback/contract/ListAnimalTest.java
+++ src/test/java/com/shelter/animalback/contract/ListAnimalTest.java
@@ -5,7 +5,8 @@ import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
-import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import com.shelter.animalback.controller.AnimalController;
 import com.shelter.animalback.domain.Animal;
@@ -17,13 +18,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 
 @ExtendWith(MockitoExtension.class)
 @Provider("AnimalShelterBack")
-@PactFolder("pacts")
-public class listAnimal {
+@PactBroker(host = "${PACT_BROKER_BASE_URL}", scheme = "${PACT_BROKER_SCHEMA}"
+        , authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
+public class ListAnimalTest {
 
     @Mock
     private AnimalService animalService;
